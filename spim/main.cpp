e3272cb05e1bad7e124f6cdcc1e47310d30f3c48
JohnAtreides <43500984+JohnAtreides@users.noreply.github.com>
2020-01-03 22:15:54 +0100

    Updated main.cpp du master (spim)

--- spim/main.cpp
+++ spim/main.cpp
@@ -12,29 +12,6 @@ DigitalOut ss(SPI_MS1, 1);
 uint32_t txbuf[20];
 uint32_t rxbuf[20];
 
-// ajout compteur de temps
-int lotOfSec = 0;
-typedef struct temps
-{
-    unsigned int hour;
-    unsigned int minute;
-    unsigned int second;
-} temps_t;
-
-temps_t cutTime(unsigned int);
-
-temps_t cutTime(unsigned int lotOfSec)
-{
-    temps_t hoMiSe; //hourMInuteSEcond..
-    hoMiSe.hour = lotOfSec / 3600;
-    lotOfSec = lotOfSec - (3600 * hoMiSe.hour);
-    hoMiSe.minute = lotOfSec / 60;
-    hoMiSe.second = hoMiSe.minute % 3600;
-
-    return hoMiSe;
-}
-// fin compteur temps
-
 // Ajout capteur T°
 void setup_temp()
 {
@@ -72,39 +49,23 @@ void done(int v)
 }
 
 volatile int i = 0;
-volatile int lot = 0;
-volatile int ho = 0;
-volatile int mi = 0;
-volatile int se = 0;
 
 int main()
 {
     spi.format(8, 3);       // 8 bits par transfert, 3 = polarité de l'horloge
-    spi.frequency(1000000); // Megabit (10^6)
+    spi.frequency(1000000); // MegaHertz (10^6)
+    int lotOfSec = 0;       // pour le compteur d'uPtime
 
     while (true)
     {
         txbuf[0] = i;
-        txbuf[4] = lot;
-        txbuf[8] = ho;
-        txbuf[12] = mi;
-        txbuf[16] = se;
+        txbuf[1] = lotOfSec;
 
-        //pour compteur timeUp
-        lotOfSec++;
-
-        temps_t result;
-        result = cutTime(lotOfSec);
-        lot = lotOfSec;
-        ho = result.hour;
-        mi = result.minute;
-        se = result.second;
-        //fin compteur
-
-        i = temp_read(); // appel la fonction qui lit la T° et met la valeur (float) dans i
+        i = temp_read();
+        lotOfSec ++; 
 
         ss = 0;
-        spi.transfer(reinterpret_cast<uint8_t *>(txbuf), 4, reinterpret_cast<uint8_t *>(rxbuf), 4, done);
+        spi.transfer(reinterpret_cast<uint8_t *>(txbuf), 8, reinterpret_cast<uint8_t *>(rxbuf), 8, done);
         ThisThread::sleep_for(1000);
     }
 }
